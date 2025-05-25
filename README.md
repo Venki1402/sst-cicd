fa1c0ed85af08a758ec7757b04eb6b8192a8c65f
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-05-25 06:27:19 +0530

    trying github action for ci and pytest
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+# CICD
+
+## commands to create a venv
+
+- python3 -m venv newvenv
+- source newvenv/bin/activate
+
+## cool things got to know
+
+- .yml extension -> YAML Ain't Markup Language
---
+++ test/test_math_utils.py
@@ -0,0 +1,16 @@
+from src.math_utils import add, divide
+
+
+def test_add():
+    assert add(1, 10) == 11
+    assert add(2, 20) == 22
+
+
+def test_add2():
+    assert add(100, 10) == 110
+    assert add(24, 200) == 224
+
+
+def test_divide():
+    assert divide(1, 10) == 0.1
+    assert divide(2, 20) == 0.1
